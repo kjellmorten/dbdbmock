ce1bed20e809ca85364fece4d41702a1c3e4e6b7
kjellmorten <post@kjellmorten.no>
2016-02-17 14:33:26 +0100

    connect and disconnect

---
+++ test/coucdb-test.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const test = require('blue-tape');
+
+const DbdbCouch = require('../lib/couchdb');
+
+// Tests
+
+test('DbdbCouch', (t) => {
+  t.equal(typeof DbdbCouch, 'function', 'should be a function');
+  t.end();
+});
+
+test('DbdbCouch.dbType', (t) => {
+  t.equal(DbdbCouch.dbType, 'couchdb', 'should be "couchdb"');
+  t.end();
+});
+
+test('DbdbCouch.isMock', (t) => {
+  t.ok(DbdbCouch.isMock, 'should be true');
+  t.end();
+});
+
+// Tests -- database connection
+
+test('db.connect', (t) => {
+  let db = new DbdbCouch();
+
+  t.equal(typeof db.connect, 'function', 'should exist');
+  t.end();
+});
+
+test('db.connect return', function (t) {
+  let db = new DbdbCouch();
+
+  return db.connect()
+
+  .then((conn) => {
+    t.equal(typeof conn, 'object', 'should return null');
+
+    db.disconnect();
+  });
+});
+
+test('db.disconnect', function (t) {
+  let db = new DbdbCouch();
+
+  t.equal(typeof db.disconnect, 'function', 'should exist');
+  t.end();
+});
